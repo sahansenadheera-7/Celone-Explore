d2eae2c59252fbcf2eef57f3eb1fe9619722b63b
IT24102257 <IT24102257@my.sliit.lk>
2026-08-17 16:07:02 +0530

    Initial commit

--- CelonExplore.Backend/src/CelonExplore.Domain/obj/Debug/net8.0/CelonExplore.Domain.AssemblyInfo.cs
+++ CelonExplore.Backend/src/CelonExplore.Domain/obj/Debug/net8.0/CelonExplore.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CelonExplore.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bda12228d3c3e519e3c7f88816df0f2e4b98799")]
 [assembly: System.Reflection.AssemblyProductAttribute("CelonExplore.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CelonExplore.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
